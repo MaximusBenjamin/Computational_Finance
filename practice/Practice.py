4d04c4d8dc86f9a07b55f5cf9586ab549e2479c2
MaximusBenjamin <mail@maxbakker.com>
2024-12-17 21:44:18 +0100

    Week 2

--- practice/Practice.py
+++ practice/Practice.py
@@ -1,4 +1,7 @@
 import random
+import math
+from collections import Counter
+import matplotlib.pyplot as plt
 
 # 100 = Integer
 # 100.0 = Float
@@ -52,4 +55,44 @@ for i in list_of_numbers:
 print(f"The sum of the number is: {sum_number}")
 """
 
+"""
+def function_fibonacci(count):
+    fibonacci_sequence = [1,1]
 
+    for i in range(2,count): 
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    return fibonacci_sequence
+
+fibonacci_numbers = function_fibonacci(10000)
+print(fibonacci_numbers)
+
+fibonacci_numbers.count([1])
+
+def extract_leading_digits(numbers):
+    leading_digits = [int(str(num)[0] for num in numbers)]
+    return leading_digits
+def benford_probabilities():
+    return {d:math.log10(1 + 1 / d) for d in range (1,10)}
+
+def analyze_fibonacci_benford(count):
+    fibonacci_numbers = function_fibonacci(count)
+
+    leading_digits = extract_leading_digits(fibonacci_numbers)
+
+    counts = Counter(leading_digits)
+
+    total = len(leading_digits) 
+    observed_frequencies = """
+
+
+x = [i+1 for i in range(10)]
+y = [(i+1)**2 for i in range(10)]
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.bar(x, y, color='red', alpha=0.2)
+ax.scatter(x, y, color='red')
+ax.plot(x, y, color='blue')
+
+plt.show()
